e148f5c68bdfcbd63010dfadee823079eaf01549
depthbomb <reesepup77@hotmail.com>
2018-04-26 15:15:06 -0500

    Updated license headers

--- core/commands/reload.js
+++ core/commands/reload.js
@@ -1,21 +1,26 @@
-/*************************************************************************
-This file is part of GoatBot!
+/*
+|--------------------------------------------------------------------------
+|	GoatBot! Automation
+|--------------------------------------------------------------------------
+|
+|	Copyright (C) 2017 - 2018 Caprine Softworks - https://caprine.net
+|
+|	This library is free software; you can redistribute it and/or
+|	modify it under the terms of the GNU Lesser General Public
+|	License as published by the Free Software Foundation; either
+|	version 2.1 of the License, or (at your option) any later version.
+|
+|	This library is distributed in the hope that it will be useful,
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+|	Lesser General Public License for more details.
+|
+|	You can receive a copy of the GNU Lesser General Public License from 
+|	http://www.gnu.org/
+|
+|--------------------------------------------------------------------------
+*/
 
-Copyright © 2017-2018 Caprine Softworks <https://caprine.net>
-
-GoatBot! licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-
-You should have received a copy of the license along with this
-work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*************************************************************************/
 exports.run = async (client, message, args, level) => {
 	if (!args || args.size < 1) return message.reply("Must provide a command to reload.");
 
