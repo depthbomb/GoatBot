813a41cbc4aec2e80047e07504864d1c238bd04d
depthbomb <reesepup77@hotmail.com>
2018-07-19 23:57:31 -0500

    Update stats command

--- core/commands/stats.js
+++ core/commands/stats.js
@@ -27,13 +27,16 @@ exports.run = (client, message, args, level) => {
 	require("moment-duration-format");
 
 	const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-	message.channel.send(`My Statistics
+	return message.channel.send(`My Statistics
+-------------
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Uptime     :: ${duration}
 • Users      :: ${client.users.size.toLocaleString()}
 • Servers    :: ${client.guilds.size.toLocaleString()}
 • Channels   :: ${client.channels.size.toLocaleString()}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+• Node       :: ${process.version}`
+
+, {code: "asciidoc"});
 };
 
 exports.conf = {
