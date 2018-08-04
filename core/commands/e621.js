3b4dbfa72bc61474618ce8e3bac46f867df1c66d
depthbomb <reesepup77@hotmail.com>
2018-08-04 01:55:19 -0500

    Fix "e621" command not properly adding NSFW-blacklisted tags to main blacklist

--- core/commands/e621.js
+++ core/commands/e621.js
@@ -29,7 +29,7 @@ exports.run = async (client, message, args, level) => {
 	const { RichEmbed } = require('discord.js');
 
 	const ratings = ["e", "q", "s", "a"];
-	let blacklistedTags = ["vore", "inflation", "gore", "macro", "scat", "watersports", "suicide", "fag", "abuse", "imminent_death", "loli", "shota", "diaper", "urine", "vomit",  "torture", "necrophilia", "castration", "hyper", "death_by_penis", "obese", "morbidly_obese", "epilepsy_warning", "feces", "flatulence", "fart", "smegma", "nightmare_fuel", "noose", "incest", "mutilation", "cheese_grater", "sensory_deprivation", "permanent_bondage", "flash", "family_guy", "death", "what", "advertisement", "what_has_science_done", "where_is_your_god_now", "male_birth", "male_pregnancy", "puffy_anus", "type:swf", "type:webm", "order:score_asc", "swastika", "nazi"];
+	let blacklistedTags = ["vore", "inflation", "gore", "macro", "scat", "watersports", "suicide", "fag", "abuse", "imminent_death", "loli", "shota", "diaper", "urine", "vomit",  "torture", "necrophilia", "castration", "hyper", "death_by_penis", "obese", "morbidly_obese", "epilepsy_warning", "feces", "flatulence", "fart", "smegma", "nightmare_fuel", "cboy", "incest", "mutilation", "cheese_grater", "sensory_deprivation", "permanent_bondage", "flash", "family_guy", "death", "what", "advertisement", "what_has_science_done", "where_is_your_god_now", "male_birth", "male_pregnancy", "puffy_anus", "type:swf", "type:webm", "order:score_asc", "swastika", "nazi", "<30_second_webm", ">30_second_webm", "no_sound"];
 	let nsfwBlacklist = ['cub', 'young', 'pregnant', 'nezumi'];	//	Tags that will be disallowed when using Q, E, and A ratings
 
 	let rating = args[0];
@@ -50,7 +50,10 @@ exports.run = async (client, message, args, level) => {
 
 	if (ratings.includes(rating) && rating.length == 1) {
 		if (rating === 'e' || rating === 'q' || rating === 'a') {
-			blacklistedTags.push(nsfwBlacklist);
+			blacklistedTags = blacklistedTags.concat(nsfwBlacklist);
+
+			console.log(blacklistedTags);
+
 			let nbl = nsfwBlacklist.map(w => {
 				return +new RegExp('\\b' + w + '\\b', 'gi').test(tags);
 			});
