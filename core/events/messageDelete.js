6b0b84057d38e2acef9804650689e1f23fcf8609
depthbomb <reesepup77@hotmail.com>
2018-08-10 20:10:48 -0500

    Update message delete event to ignore deleted bot messages

--- core/events/messageDelete.js
+++ core/events/messageDelete.js
@@ -23,7 +23,7 @@
 
 module.exports = (client, message) => {
 	//	Don't log our own messages
-	if (message.author.id === client.user.id || message.channel.type === 'dm') return;
+	if (message.author.id === client.user.id || message.channel.type === 'dm' || message.author.bot) return;
 
 	let logMessage;
 	let isAttachment = false;
