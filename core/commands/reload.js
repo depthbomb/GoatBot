36ccde268563e4c18f1903b20f8a5269b6cdf687
depthbomb <reesepup77@hotmail.com>
2018-08-13 14:16:06 -0500

    Fix problem when attempting to reload config

--- core/commands/reload.js
+++ core/commands/reload.js
@@ -28,7 +28,7 @@ exports.run = async (client, message, args, level) => {
 	const item = args[0];
 
 	if (item === 'config') {
-		const configPath = path.join(client.rootPath, 'config.json');
+		const configPath = path.join(client.rootPath, 'config.js');
 		delete require.cache[require.resolve(configPath)];
 		client.config = require(configPath);
 		return message.reply('Configuration has been reloaded.');
