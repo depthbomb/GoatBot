bbdca205a3d387a6d000abaa5a7b255f415dc7b0
depthbomb <reesepup77@hotmail.com>
2018-09-01 17:18:04 -0500

    Update "unbox" command extra_info to support changing weights Update guildMemberAdd message for non-deported users

--- core/events/guildMemberAdd.js
+++ core/events/guildMemberAdd.js
@@ -36,7 +36,7 @@ module.exports = (client, member) => {
 			if (!client.deported.includes(mem.id)) {
 				client.log("bot", `Sending welcome DM to new user, ${member.user.tag}.`);
 	
-				mem.send(`**Hey, ${mem.user.username}!** Welcome to the Cyan.TF Discord server.\n\nYou may notice that you can't chat with the other users in the normal channels. For security and abuse reasons, you have been given a temporary role that separates you from the other users. But don't worry! You will automatically be given access to the rest of the server after 5 minutes. If you are lucky, an admin will give you the role quicker so you can jump right in!\n\nIn the mean time, you can send messages in the \`#refugee-camp\` channel. Also, make sure to read the rules and other info in the \`#rules\` channel at the top.\n\n_We are glad to have you in the server!\nHave fun!_`);
+				mem.send(`**Hey, ${mem.user.username}!** Welcome to the Cyan.TF Discord server.\n\n**You may notice that you can't see other users or channels. Do not worry!** For security and abuse reasons, you have been given a temporary role that separates you from the other users and channels. You will automatically be given access to the rest of the server after 5 minutes. If you are lucky, an admin will give you the role quicker so you can jump right in!\n\nIn the mean time, you can send messages in the \`#refugee-camp\` channel. Also, make sure to read the rules and other info in the \`#rules\` channel at the top.\n\n_We are glad to have you in the server!\nHave fun!_`);
 			
 				/**
 				* Remove the restricted role and give the user the member role after the provided delay
