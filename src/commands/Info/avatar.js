26cd04fac8dddac994633ed37895908892b9703b
depthbomb <reesepup77@hotmail.com>
2019-12-06 00:25:07 -0600

    Add avatar command

---
+++ src/commands/Info/avatar.js
@@ -0,0 +1,81 @@
+/*
+|--------------------------------------------------------------------------
+|	GoatBot! Automation
+|--------------------------------------------------------------------------
+|
+|	Copyright (C) 2017 - 2020 Caprine Logic - https://caprine.net
+|
+|	This library is free software; you can redistribute it and/or
+|	modify it under the terms of the GNU Lesser General Public
+|	License as published by the Free Software Foundation; either
+|	version 2.1 of the License, or (at your option) any later version.
+|
+|	This library is distributed in the hope that it will be useful,
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+|	Lesser General Public License for more details.
+|
+|	You can receive a copy of the GNU Lesser General Public License from 
+|	http://www.gnu.org/
+|
+|--------------------------------------------------------------------------
+*/
+
+exports.run = async (client, message, args, level) => {
+	if (!args || args.length !== 1) return;
+	const { RichEmbed } = require('discord.js');
+	const mention = args[0];
+	if (mention.match(/<@!?\d{17,19}>/g)) {
+		member = message.mentions.members.first();
+	} else {
+		member = message.guild.members.find(m => m.id === mention);
+	}
+
+	if (member) {
+		const defaultAvatarUrl = member.user.defaultAvatarURL;
+		const displayAvatarUrl = member.user.displayAvatarURL;
+		const avatarURL        = member.user.avatarURL;
+		const avatar           = member.user.avatar;
+
+		const urls = [
+			`[Avatar](${displayAvatarUrl})`,
+			`[Default Avatar](${defaultAvatarUrl})`
+		].join('\n');
+
+		const embed = new RichEmbed()
+			  .setTitle(`${member.displayName}'s avatars`)
+			  .setDescription(`Avatar ID: \`${avatar}\``)
+			  .setColor(client.colors.blue)
+			  .setImage(displayAvatarUrl)
+			  .setThumbnail(defaultAvatarUrl)
+			  .addField('URLs', urls);
+
+		return message.reply({ embed });
+	} else {
+		return message.reply(`Could not find member.`);
+	}
+};
+
+exports.conf = {
+	enabled: true,
+	aliases: [
+		'pfp',
+		'ava',
+		'profilepicture'
+	],
+	permLevel: 0,
+	deleteTrigger: false,
+};
+
+exports.help = {
+	name: "avatar",
+	category: "Info",
+	description: "Retrieves the URL to a user's Discord avatar",
+	usage: "avatar [@mention|user ID]",
+	params: {
+		"@mention|user ID": "Mention or user ID to retrieve the avatar of"
+	},
+	examples: [
+		"avatar @Username#0000"
+	]
+};
