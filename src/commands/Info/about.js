c7877c976529aedfec629e3521b15112b32b6544
depthbomb <reesepup77@hotmail.com>
2019-12-10 16:36:26 -0600

    Refactor about command

--- src/commands/Info/about.js
+++ src/commands/Info/about.js
@@ -21,64 +21,53 @@
 |--------------------------------------------------------------------------
 */
 
+const { RichEmbed } = require('discord.js');
 exports.run = async (client, message, args, level) => {
-	const { RichEmbed } = require('discord.js');
-	if (!args[0]) {
-		let aboutEmbed = new RichEmbed()
-			.setAuthor("About GoatBot!", client.user.avatarURL)
-			.setColor(client.config.color)
-			.setDescription(`I am a general purpose Discord bot with a variety of fun, informative, and moderative commands as well as useful statistical functions. I am owned and developed by <@${client.config.ownerId}> using the [Discord.js Node module](https://discord.js.org/).\n\I'm still a work-in-progress and new features as well as commands are still being developed. If you would like something to be added, contact <@${client.config.ownerId}> here on Discord, [Steam](https://steamcommunity.com/id/minorin), or make a post in the [Issue Tracker.](https://github.com/depthbomb/GoatBot/issues)`)
-			.addField("Message from my creator", "GoatBot is maintained and hosted 100% by me out of pocket in my spare time. Like what I do? [Support me!](https://paypal.me/depthbomb)")
-			.addField("More Info",
-				"`!about cooldowns`\n" +
-				"`!about logging`\n"
-			)
-			.setFooter(`GoatBot ${client.config.botVersion} by depthbomb#7698`);
-		return message.author.send({
-			embed: aboutEmbed
-		});
-	} else if (args && args[0] == "cooldowns") {
-		let featuresEmbed = new RichEmbed()
-			.setAuthor("About GoatBot | Cooldowns", client.user.avatarURL)
-			.setColor(client.config.color)
-			.setDescription(`All commands are susceptible to some sort per-user of delay or **cooldown**. This cooldown is to prevent abuse of the bot via command spamming and to limit some commands that rely on a third-party API that may have a request limit. By default, the cooldown of individual commands is ${client.config.cooldowns.default}s.\n\nSome commands may have a **global cooldown** where everyone shares the cooldown. To see what a command's cooldown is and if it has a global cooldown, type \`!help <command name>\`.`)
-			.setFooter(`GoatBot ${client.config.botVersion} by depthbomb#7698`);
-		return message.author.send({
-			embed: featuresEmbed
-		});
-	} else if (args && (args[0] == "logging" || args[0] == "logs")) {
-		let loggingEmbed = new RichEmbed()
-			.setAuthor("About GoatBot | Logging", client.user.avatarURL)
-			.setColor(client.config.color)
-			.setDescription(`I log all messages and events in channels that I have access to. However, I will keep this info private as it is used for general moderation and statistical purposes. If you wish to have entries related to you removed, please contact my owner.`)
-			.setFooter(`GoatBot ${client.config.botVersion} by depthbomb#7698`);
-		return message.author.send({
-			embed: loggingEmbed
-		});
-	} else {
-		return;
+	const section = args.join(' ').toLowerCase() || 'main';
+	const embed = new RichEmbed()
+		  .setAuthor('About GoatBot!', client.user.avatarURL)
+		  .setColor(client.colors.brand)
+		  .setFooter(`By depthbomb#0163`);
+	switch (section) {
+		default:
+		case 'main':
+			embed.setDescription(`I am a general purpose Discord bot with a variety of fun, informative, and moderative commands as well as useful statistical functions. I am owned and developed by <@${client.config.ownerId}> using the [Discord.js library](https://discord.js.org/).\n\I'm still a work-in-progress and new features as well as commands are still being developed. If you would like something to be added, contact <@${client.config.ownerId}> here on Discord, [Steam](https://steamcommunity.com/id/minorin), or make a post in the [Issue Tracker.](https://github.com/depthbomb/GoatBot/issues)`)
+			.addField('Message from my creator', 'GoatBot is maintained and hosted 100% by me out of pocket in my spare time. Like what I do? [Support me!](https://paypal.me/depthbomb)')
+			.addField('More Info', `\`${client.printCmd('about')} cooldowns\`\n\`${client.printCmd('about')} logging\``);
+			break;
+		case 'cooldown':
+		case 'cooldowns':
+			embed.setDescription(`All commands are susceptible to some sort per-user of delay or **cooldown**. This cooldown is to prevent abuse of the bot via command spamming and to limit some commands that rely on a third-party API that may have a request limit. By default, the cooldown of individual commands is ${client.config.cooldowns.default}s.\n\nSome commands may have a **global cooldown** where everyone shares the cooldown. To see what a command's cooldown is and if it has a global cooldown, type \`!help <command name>\`.`);
+			break;
+		case 'logging':
+		case 'log':
+		case 'logs':
+				embed.setDescription(`I log all messages and events in channels that I have access to. However, I will keep this info private as it is used for general moderation and statistical purposes. If you wish to have entries related to you removed, please contact my owner.`);
+			break;
 	}
+
+	return message.channel.send({ embed });
 };
 
 exports.conf = {
 	enabled: true,
 	aliases: [
-		"abt",
-		"info"
+		'abt',
+		'info'
 	],
 	permLevel: 0
 };
 
 exports.help = {
-	name: "about",
-	category: "System",
-	description: "Shows info about me",
-	usage: "about [section?]",
+	name: 'about',
+	category: 'Info',
+	description: 'Shows info about me',
+	usage: 'about [section?]',
 	params: {
-		"section": "(Optional) Section to read about"
+		'section': '(Optional) Section to read about'
 	},
 	examples: [
-		"about",
-		"about cooldowns"
+		'about',
+		'about cooldowns'
 	]
 };
