249915ba56b26efd846e3fd16941869c94ed42b7
depthbomb <reesepup77@hotmail.com>
2019-12-18 17:34:53 -0600

    Fix a crash related to stats command

--- src/commands/Info/stats.js
+++ src/commands/Info/stats.js
@@ -21,17 +21,17 @@
 |--------------------------------------------------------------------------
 */
 
-const moment = require('moment');
+const ms = require('ms');
 exports.run = (client, message, args, level) => {
-	const duration = moment.duration(client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+	const duration = ms(client.uptime, { long: true });
 	return message.channel.send(`My Statistics
 -------------
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Node       :: ${process.version}`
+• Mem Usage :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime    :: ${duration}
+• Users     :: ${client.users.size.toLocaleString()}
+• Servers   :: ${client.guilds.size.toLocaleString()}
+• Channels  :: ${client.channels.size.toLocaleString()}
+• Node      :: ${process.version}`
 
 , { code: 'asciidoc' });
 };
