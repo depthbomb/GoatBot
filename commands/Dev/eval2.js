d1b595293a5445399be01591c257284589805621
depthbomb <reesepup77@hotmail.com>
2020-07-24 13:16:41 -0500

    Update NPM deps

---
+++ commands/Dev/eval2.js
@@ -0,0 +1,62 @@
+/*
+|--------------------------------------------------------------------------
+|	GoatBot! Automation
+|--------------------------------------------------------------------------
+|
+|	Copyright (C) 2017 - 2020 Caprine Logic - https://caprine.net
+|
+|	This library is free software; you can redistribute it and/or
+|	modify it under the terms of the GNU Lesser General Public
+|	License as published by the Free Software Foundation; either
+|	version 2.1 of the License, or (at your option) any later version.
+|
+|	This library is distributed in the hope that it will be useful,
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+|	Lesser General Public License for more details.
+|
+|	You can receive a copy of the GNU Lesser General Public License from 
+|	http://www.gnu.org/
+|
+|--------------------------------------------------------------------------
+*/
+
+const trunc = require('truncate');
+exports.run = async (client, message, args, level) => {
+	if(args.length === 0) return;
+	const code = args.join(' ');
+
+	try {
+		const start = process.hrtime();
+		const execTime = process.hrtime(start);
+		let evaled = eval(code);
+		let clean = await client.clean(client, evaled);
+
+		if (typeof evaled !== 'string')
+			evaled = require('util').inspect(evaled);
+	} catch (err) {
+		return message.reply(`\`ERROR\` \`\`\`js\n${err}\n\`\`\``);
+	}
+};
+
+exports.conf = {
+	enabled: true,
+	cooldown: 2,
+	aliases: [
+		'ev2'
+	],
+	permLevel: 5
+};
+
+exports.help = {
+	name: 'eval2',
+	category: 'Dev',
+	description: 'Evaluates arbitrary JavaScript code.',
+	usage: 'eval2 [code]',
+	params: {
+		'code': 'JavaScript code to evaluate'
+	},
+	examples: [
+		'eval2 [\'test\', \'test2\'].join(\', \')'
+	]
+};
