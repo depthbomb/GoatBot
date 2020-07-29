003ca61806f8d861d3cbd028943f0f9d96eb7465
depthbomb <reesepup77@hotmail.com>
2020-07-28 20:54:21 -0500

    Update ping command

--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -1,11 +1,36 @@
+/*
+|--------------------------------------------------------------------------
+|	GoatBot! Automation
+|--------------------------------------------------------------------------
+|
+|	Copyright (C) 2017 - 2020 Caprine Logic - https://caprine.net
+|
+|	This library is free software; you can redistribute it and/or
+|	modify it under the terms of the GNU Lesser General Public
+|	License as published by the Free Software Foundation; either
+|	version 2.1 of the License, or (at your option) any later version.
+|
+|	This library is distributed in the hope that it will be useful,
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+|	Lesser General Public License for more details.
+|
+|	You can receive a copy of the GNU Lesser General Public License from 
+|	http://www.gnu.org/
+|
+|--------------------------------------------------------------------------
+*/
+
+const { MessageEmbed } = require('discord.js');
 exports.run = async (client, message, args, level) => {
-	const { MessageEmbed } = require('discord.js');
-	let embed = new MessageEmbed().setDescription('Testing...');
+	const embed = new MessageEmbed()
+		  .setDescription('Testing...');
+
 	const msg = await message.channel.send({ embed });
-	embed = new MessageEmbed()
+
+	embed
 		.setColor('RANDOM')
-		.setDescription(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
-	;
+		.setDescription(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 
 	return msg.edit({ embed });
 };
