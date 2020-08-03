312bb811c992a0b7b5de8930386fc622a2deeb30
depthbomb <reesepup77@hotmail.com>
2020-08-03 18:27:28 -0500

    Refactor e621 command

--- commands/NSFW/e621.js
+++ commands/NSFW/e621.js
@@ -21,123 +21,105 @@
 |--------------------------------------------------------------------------
 */
 
+const ratings = ['e', 'q', 's'];
+const nsfwBlacklist = ['cub', 'young', 'pregnant', 'nezumi', 'mei_(one_stormy_night)'];	//	Tags that will be disallowed when using Q, E, and A ratings
+const tagBlacklist = ['male_lactation', 'vore', 'inflation', 'gore', 'macro', 'scat', 'watersports', 'suicide', 'fag', 'abuse', 'imminent_death', 'loli', 'shota', 'diaper', 'urine', 'vomit',  'torture', 'necrophilia', 'castration', 'hyper', 'death_by_penis', 'obese', 'morbidly_obese', 'epilepsy_warning', 'feces', 'flatulence', 'fart', 'smegma', 'nightmare_fuel', 'cboy', 'incest', 'mutilation', 'cheese_grater', 'sensory_deprivation', 'permanent_bondage', 'flash', 'family_guy', 'death', 'what', 'advertisement', 'what_has_science_done', 'where_is_your_god_now', 'male_birth', 'male_pregnancy', 'puffy_anus', 'type:swf', 'type:webm', 'order:score_asc', 'swastika', 'nazi', '<30_second_webm', '>30_second_webm', 'no_sound'];
+
 const trunc = require('truncate');
 const request = require('request');
 const { MessageEmbed } = require('discord.js');
+const { MissingArgumentError, InvalidArgumentError, InvalidCommandLocationError } = require('@errors');
 exports.run = async (client, message, args, level) => {
-	if (args.length === 0) return;
-	if (!message.channel.nsfw)
-		return client.msg(message, 'red', 'error', 'This command can only be used in NSFW channels.');
+	MissingArgumentError.assert(args.length >= 3, 'You must provide three arguments');
+	InvalidCommandLocationError.assert(message.channel.nsfw, 'This command can only be used in NSFW channels')
 
-	const ratings = ['e', 'q', 's', 'a'];
-	let blacklistedTags = ['male_lactation', 'vore', 'inflation', 'gore', 'macro', 'scat', 'watersports', 'suicide', 'fag', 'abuse', 'imminent_death', 'loli', 'shota', 'diaper', 'urine', 'vomit',  'torture', 'necrophilia', 'castration', 'hyper', 'death_by_penis', 'obese', 'morbidly_obese', 'epilepsy_warning', 'feces', 'flatulence', 'fart', 'smegma', 'nightmare_fuel', 'cboy', 'incest', 'mutilation', 'cheese_grater', 'sensory_deprivation', 'permanent_bondage', 'flash', 'family_guy', 'death', 'what', 'advertisement', 'what_has_science_done', 'where_is_your_god_now', 'male_birth', 'male_pregnancy', 'puffy_anus', 'type:swf', 'type:webm', 'order:score_asc', 'swastika', 'nazi', '<30_second_webm', '>30_second_webm', 'no_sound'];
-	let nsfwBlacklist = ['cub', 'young', 'pregnant', 'nezumi', 'mei_(one_stormy_night)'];	//	Tags that will be disallowed when using Q, E, and A ratings
+	const rating = args[0];
 
-	let rating = args[0];
-	let pageNum = args[1];
-	let tags = args.slice(2).join(" ").toLowerCase();
+	InvalidArgumentError.assert(ratings.includes(rating), `The rating you provided is invalid. Valid ratings are **${ratings.join(', ')}**`);
 
-	let bl = blacklistedTags.map(w => {
-		return +new RegExp('\\b' + w + '\\b', 'gi').test(tags);
-	});
+	const pageNum = parseInt(args[1]);
 
-	if (bl.indexOf(1) >= 0) return client.msg(message, 'red', 'error', 'Your tags contain blacklisted terms.');
+	InvalidArgumentError.assert(pageNum !== NaN, 'Page number must be a number');
+	InvalidArgumentError.assert(pageNum <= 750 && pageNum >= 1, 'Page number must be a between 1 and 750');
 
-	if (rating == null) return client.msg(message, 'red', 'error', 'Rating argument is required');
-	if (isNaN(pageNum)) return client.msg(message, 'red', 'error', 'Page must be a number');
-	if (pageNum > 750 || pageNum < 1) return client.msg(message, 'red', 'error', 'Page number must be greater than 0 and less than 750');
-	if (tags == null) return client.msg(message, 'red', 'error', 'Tags are required');
-	if (tags.split(" ").length > 5) return client.msg(message, 'red', 'error', 'You may only search 5 tags at a time.');
+	let tags = args.slice(2).join(' ').toLowerCase().split(' ');
 
-	if (ratings.includes(rating) && rating.length == 1) {
-		if (rating === 'e' || rating === 'q' || rating === 'a') {
-			blacklistedTags = blacklistedTags.concat(nsfwBlacklist);
+	let blacklistedTags = [...tagBlacklist];
+	if (rating !== 's') {
+		blacklistedTags = blacklistedTags.concat(nsfwBlacklist);
+	}
 
-			let nbl = nsfwBlacklist.map(w => {
-				return +new RegExp('\\b' + w + '\\b', 'gi').test(tags);
-			});
+	const hasBlacklistedTags = blacklistedTags.filter(t => tags.includes(t)).length > 0;
 
-			if (nbl.indexOf(1) >= 0) return client.msg(message, 'red', 'error', 'Your query contains tags that may not be used with *Q*, *E*, or *A* ratings.');
+	InvalidArgumentError.assert(!hasBlacklistedTags, 'Your tags contain blacklisted terms');
+
+	let maxAttempts = 5;
+	let attempts = 0;
+	const msg = await message.channel.send('Sending request...');
+
+	const searchQuery = `tags=${tags}+rating:${rating}&limit=320&page=${pageNum}`;
+	const apiUrl = `https://e621.net/posts.json?${searchQuery}`;
+
+	request({
+		headers: { 'User-Agent': 'GoatBot! Discord Automaton for Caprine Logic (depthbomb @ e621, depthbomb#0163 @ Discord)' },
+		uri: apiUrl,
+		method: 'GET'
+	}, (err, res, body) => {
+		if (err) return msg.edit('There was a problem when requesting API data. Please try again.');
+
+		const b = body;
+		let data;
+
+		try {
+			data = JSON.parse(b).posts;
+		} catch (error) {
+			return msg.edit('Invalid response from the e621 API. Is the website down?');
 		}
 
-		let maxAttempts = 5;
-		let attempts = 0;
-		let msg = await message.channel.send('Sending request...');
-		tags = tags.replace(/rating:.*/g, '');	//	Remove any rating tags the user adds
+		msg.edit('API data retrieved, processing...');
 
-		const searchQuery = `tags=${tags}+rating:${rating}&limit=320&page=${pageNum}`;
-		const apiUrl = `https://e621.net/post/index.json?${searchQuery}`;
+		const sendPost = (d) => {
+			if (d.success && d.success === false) return msg.edit(`<@${message.author.id}>, Error: ` + d.message);
+			if (d === undefined || d.length == 0) return msg.edit(`<@${message.author.id}>, No results for your query. Make sure the tags you used exist and are formatted correctly. If the tags are correct then try lowering the page number. If all else fails, then there might just not be any results for what you searched for.\n¯\\_(ツ)_/¯`);
 
-		request({
-			headers: {
-				'User-Agent': client.config.userAgent
-			},
-			uri: apiUrl,
-			method: 'GET'
-		}, (err, res, body) => {
-			if (err) return msg.edit('There was a problem when requesting API data. Please try again.');
+			const selected = data.shuffle()[0];
+			const postTags = selected.tags.general;
+			const postHasBlacklistedTags = blacklistedTags.filter(t => postTags.includes(t)).length > 0;
 
-			const b = body;
-			let data;
-
-			try {
-				data = JSON.parse(b);
-			} catch (error) {
-				return msg.edit('Invalid response from the e621 API. Is the website down?');
-			}
-
-			msg.edit('API data retrieved, processing...');
-
-			let sendPost = (d) => {
-				if (d.success && d.success === false) return msg.edit(`<@${message.author.id}>, Error: ` + d.message);
-				if (d === undefined || d.length == 0) return msg.edit(`<@${message.author.id}>, No results for your query. Make sure the tags you used exist and are formatted correctly. If the tags are correct then try lowering the page number. If all else fails, then there might just not be any results for what you searched for.\n¯\\_(ツ)_/¯`);
-
-				let selected = data.shuffle()[0];
-				let postTags = selected.tags.split(" ");
-				let blT = blacklistedTags.map((w) => {
-					return +new RegExp('\\b' + w + '\\b', 'gi').test(postTags);
-				});
-
-				if (attempts < maxAttempts) {
-					if (blT.indexOf(1) >= 0) {
-						attempts++;
-						msg.edit(`Blacklisted post found... trying again... (${attempts} out of ${maxAttempts})`);
-						client.setTimeout(() => sendPost(data), 1000);
-						return;
-					} else {
-						let postEmbed = new MessageEmbed()
-							.setAuthor('E621', 'https://e621.net/apple-touch-icon.png', 'https://e621.net/')
-							.setImage(selected.file_url)
-							.setDescription(`https://e621.net/post/show/${selected.id}/`)
-							.addField('Tags', '```' + trunc(selected.tags, 1000, {ellipsis: "..."}) + '```')
-							.addField('Artist(s)', selected.artist.length > 0 ? "`" + selected.artist.join(", ") + "`" : 'unknown_artist')
-							.setFooter(`${data.length < 320 ? data.length : '>' + data.length} results`)
-							.setColor('#002d55');
-
-							if (selected.description) {
-								const desc = `${trunc(selected.description, 1000, {ellipsis: "..."})}`;
-								postEmbed.addField('Description', `${selected.description !== "" ? desc : '_No description_'}`)
-							}
-
-						return msg.edit(`<@${message.author.id}>`, {
-							embed: postEmbed
-						});
-					}
+			if (attempts < maxAttempts) {
+				if (postHasBlacklistedTags) {
+					attempts++;
+					msg.edit(`Blacklisted tag(s) found in post, trying again in 1 second... (${attempts} out of ${maxAttempts})`);
+					client.setTimeout(() => sendPost(data), 1000);
+					return;
 				} else {
-					return msg.edit('Attempt limit reached. Please try again.');
-				}
-			};
+					const embed = new MessageEmbed()
+						.setAuthor('E621', 'https://e621.net/apple-touch-icon.png', 'https://e621.net/')
+						.setImage(selected.file.url)
+						.setDescription(`https://e621.net/post/show/${selected.id}`)
+						.addField('Artist(s)', selected.tags.artist.length > 0 ? "`" + selected.tags.artist.join(', ') + "`" : 'unknown_artist')
+						.setFooter(`${data.length < 320 ? data.length : '>' + data.length} results`)
+						.setColor('#002d55');
 
-			sendPost(data);
-		});
-	} else {
-		return client.msg(message, 'red', 'error', `Invalid rating provided. Valid ratings are **${ratings.join(', ')}**.`);
-	}
+						if (selected.description) {
+							const desc = `${trunc(selected.description, 1000, {ellipsis: "..."})}`;
+							embed.addField('Description', `${selected.description !== "" ? desc : '_No description_'}`)
+						}
+
+					return msg.edit(null, { embed });
+				}
+			} else {
+				return msg.edit('Attempt limit reached. Please try again.');
+			}
+		};
+
+		sendPost(data);
+	});
 };
 
 exports.conf = {
 	enabled: true,
-	cooldown: 6,
+	cooldown: 5,
 	globalCd: true,
 	aliases: [
 		'e6',
@@ -153,13 +135,13 @@ exports.help = {
 	description: 'Command for getting data from e621.net.',
 	usage: 'e621 [rating] [page] [tags]',
 	params: {
-		'rating': 'Rating to look for, can be \'s\', \'q\', \'e\', or \'a\'',
+		'rating': 'Rating to look for, can be \'s\', \'q\', or \'e\'',
 		'page': 'Page number to search in. Max of 750. You are likely to find no results in very high page numbers so stick to around 1 to 10',
-		'tags': 'Tags used when finding posts, max of 5 tags'
+		'tags': 'Tags used when finding posts'
 	},
 	examples: [
 		'e621 s 2 fluffy',
-		'yiff e 3 butt',
-		'e6 a 3 goat cute'
+		'e621 e 3 butt',
+		'e621 q 1 goat cute'
 	]
 };
