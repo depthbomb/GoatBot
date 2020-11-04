de2b9b45bf550a86ddd7456333e516cc075df125
depthbomb <reesepup77@hotmail.com>
2020-11-03 19:51:35 -0600

    Update tasks

--- tasks/clearLockdowns.js
+++ tasks/clearLockdowns.js
@@ -27,7 +27,7 @@ module.exports = client => {
 		name: 'clearLockdowns',
 		description: 'Clears expired channel lockdowns',
 		enabled: true,
-		interval: 5,
+		interval: 10,
 		action: () => {
 			if (client.online) {
 				const lockdowns = client.store.lockdowns;
